87ad1b9837dea757c010fb62d2b6f4c7b6498445
Dan Daly <dan.john.daly@gmail.com>
2017-09-29 19:16:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(arr, el) {
-  var new_arr = arr
-  return new_arr.unshift(el)
+  let new_arr = arr
+  new_arr = new_arr.unshift(el)
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, el) {
   arr = arr.unshift(el)
   return arr
+}
+
+function addElementToEndOfArray(arr, el) {
+  let new_arr = arr
+  new_arr = new_arr.push(el)
+  return new_arr
 }
